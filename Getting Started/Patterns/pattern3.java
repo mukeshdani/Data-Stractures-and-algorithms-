c3f0f18f6309ea9ad64f9bd12ec6b641a69f06ae
mukeshdani <mukeshdani00@gmail.com>
2021-11-02 09:40:03 +0530

    add 3rd pattern

---
+++ Getting Started/Patterns/pattern3.java
@@ -0,0 +1,29 @@
+//1. You are given a number n.
+//2. You've to create a pattern of * and separated by tab as shown in output format.
+
+//                  *	
+//              *	*	
+//         *	*	*	
+//      *	*	*	*	
+// *	*	*	*	*	
+
+import java.util.*;
+
+public class pattern3 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+       int n= scn.nextInt();
+        // write ur code here
+         for( int i = 1 ; i<=n; i++){
+             
+             for ( int j = 1 ; j<=n-i;j++){
+                 System.out.print("\t");
+             }
+             for( int j =1; j<=i;j++ ){
+                 System.out.print("*\t");
+             }
+             System.out.println();
+         }
+    }
+}
