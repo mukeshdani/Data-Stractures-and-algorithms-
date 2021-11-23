217358eee5f826fff95bd88e43543d7619b598b5
Mukesh Dani <mukeshdani00@gmail.com>
2021-11-23 22:43:00 +0530

    add String Interning

---
+++ Level 1/String, StringBuilder and arraylist/StringInterning.java
@@ -0,0 +1,31 @@
+// String Interning 
+// difference between == and equals()
+// Imutability 
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+	    String str = "pepcoder";
+        String str2 = new String("hello");
+		String str3 ="hello";
+        String str4 = new String ("hello");
+        String str5 = "hello";
+        
+
+        System.out.println(str2==str3);
+        System.out.println(str3==str5);
+        System.out.println(str2==str4);
+        System.out.println(str2.equals(str3));
+        System.out.println(str3.equals(str5));
+        System.out.println(str2.equals(str4));
+        System.out.println(str==str2);
+        System.out.println(str.equals(str2));
+
+	}
+
+}
