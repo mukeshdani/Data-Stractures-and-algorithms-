3762cf6e2e8d43b11c5a36362b9ec27894b296f9
Mukesh Dani <mukeshdani00@gmail.com>
2021-11-28 17:58:41 +0530

    add java code

--- Contests/FJP-2 Contest-05/FunnyString.cpp
+++ Contests/FJP-2 Contest-05/FunnyString.cpp
@@ -1,6 +1,80 @@
 // Funny String
 
 //Solution 
+
+
+
+// JAVA CODE 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+    // Write your code here
+        int len=s.length();
+        int j=len-2;
+        int i;
+      
+         for(i=1;i<len;i++,j--)
+            {
+            if(Math.abs(s.charAt(i)-s.charAt(i-1))!=Math.abs(s.charAt(j)-s.charAt(j+1))){
+                return "Not Funny";
+               
+            }
+        }
+        if(i==len){
+            return "Funny";
+        }
+    return null;}
+}
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.funnyString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
+// CPP CODE 
 #include <cmath>
 #include <cstdio>
 #include <vector>
