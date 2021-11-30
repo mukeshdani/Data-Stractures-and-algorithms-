df1c619f493da9ceb3f082a35ebcd3bfde101d30
Mukesh Dani <mukeshdani00@gmail.com>
2021-11-30 23:46:11 +0530

    add today work

---
+++ Level 1/Recursion Array/First Index.java
@@ -0,0 +1,77 @@
+
+//First Index
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner (System.in );
+        int n = scn.nextInt();
+        
+
+        int [] arr = new int [n];
+        for ( int i = 0 ; i<n ;i++){
+            arr[i]=scn.nextInt();
+
+        }
+int x= scn.nextInt();
+        System.out.println(firstIndex(arr,0,x));
+    }
+
+    public static int firstIndex(int[] arr, int idx, int x){
+
+     if (idx == arr.length)return -1;
+        int Fi = firstIndex(arr , idx+1,x);
+
+        if ( arr[idx]==x){
+            return idx;
+        }else return Fi;
+    }
+
+}
+
+/*1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are given a number x. 
+4. You are required to find the first index at which x occurs in array a.
+5. If x exists in array, print the first index where it is found otherwise print -1.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+Constraints
+
+1 
+
+Format
+
+Input
+
+A number n
+n1
+n2
+.. n number of elements
+A number x
+
+Output
+
+A number representing first index of occurence of x in array a or -1 if not found at all.
+
+Example
+
+Sample Input
+
+6
+15
+11
+40
+4
+4
+9
+4
+
+Sample Output
+
+3*/
