0106ac799547edd3871a706ac5228660af845632
Mukesh Dani <mukeshdani00@gmail.com>
2021-12-01 20:45:58 +0530

    change

--- Level 1/Recursion Array/Last Index.java
+++ Level 1/Recursion Array/Last Index.java
@@ -32,8 +32,9 @@ int x= scn.nextInt();
 
 
 
+/*
 
-    /*1. You are given a number n, representing the count of elements.
+1. You are given a number n, representing the count of elements.
 2. You are given n numbers.
 3. You are given a number x. 
 4. You are required to find the last index at which x occurs in array a.
