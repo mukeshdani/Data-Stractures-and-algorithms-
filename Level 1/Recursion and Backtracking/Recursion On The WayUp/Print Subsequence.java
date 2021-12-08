92b46068c00639351910adbece2fac2b37415a6c
Mukesh Dani <mukeshdani00@gmail.com>
2021-12-09 00:35:29 +0530

    work

--- Level 1/Recursion and Backtracking/Recursion On The WayUp/Print Subsequence.java
+++ Level 1/Recursion and Backtracking/Recursion On The WayUp/Print Subsequence.java
@@ -1,7 +1,39 @@
 
 //Print Subsequence
 
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+   Scanner scn = new Scanner (System.in );
+   String input = scn.next();
+
+   printSS(0,input,"");
+
+
+    }
+
+    public static void printSS(int idx ,String input, String output) {
+  if ( idx == input.length()){
+      System.out.println(output);
+      return ;
+  }
+
+  char ch = input.charAt(idx);
+
+  //yes 
+
+  printSS(idx+1 , input,output+ch);
+
+  //no
+
+  printSS(idx+1,input,output);
+
+    }
+
+}
 
 
 
