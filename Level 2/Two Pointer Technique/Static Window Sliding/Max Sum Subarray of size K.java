2b591827b31ad6702ffdfe1bd1359aa82122acce
Mukesh Dani <mukeshdani00@gmail.com>
2022-01-08 15:31:43 +0530

    pointer

---
+++ Level 2/Two Pointer Technique/Static Window Sliding/Max Sum Subarray of size K.java
@@ -0,0 +1,105 @@
+//Max Sum Subarray of size K 
+//Time --> O(N)
+//Space --> O(1);
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
+  
+            // Appending new elements at 
+            // the end of the list 
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                Arr.add(x); 
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static int maximumSumSubarray(int K, ArrayList<Integer> arr,int N){
+        // code here
+     
+        int currSum  = 0 ;
+        for ( int i = 0 ; i<K ; i++ ){
+            currSum += arr.get(i);
+        }
+        
+        int maxSum = currSum;
+        
+        for ( int i = K ; i< N ; i++){
+            int inc = arr.get(i);
+            int exc = arr.get(i-K);
+            currSum = currSum + inc - exc ;
+            maxSum = Math.max(maxSum , currSum);
+        }
+        return maxSum;
+    }
+}
+
+
+
+/*Given an array of integers Arr of size N and a number K. Return the maximum sum of a subarray of size K.
+
+ 
+
+Example 1:
+
+Input:
+N = 4, K = 2
+Arr = [100, 200, 300, 400]
+Output:
+700
+Explanation:
+Arr3  + Arr4 =700,
+which is maximum.
+ 
+
+Example 2:
+
+Input:
+N = 4, K = 4
+Arr = [100, 200, 300, 400]
+Output:
+1000
+Explanation:
+Arr1 + Arr2 + Arr3  
++ Arr4 =1000,
+which is maximum.
+ 
+
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function maximumSumSubarray() which takes the integer k, vector Arr with size N, containing the elements of the array and returns the maximum sum of a subarray of size K.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+ 
+
+
+Constraints:
+1<=N<=105
+1<=K<=N
+ 
+
+*/
