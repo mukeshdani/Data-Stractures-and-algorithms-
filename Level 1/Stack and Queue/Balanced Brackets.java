7173a2c87fd5da47fb45ddc826e114f450945cb5
Mukesh Dani <mukeshdani00@gmail.com>
2022-02-01 23:44:55 +0530

    Stack and Queue

--- Level 1/Stack and Queue/Balanced Brackets.java
+++ Level 1/Stack and Queue/Balanced Brackets.java
@@ -1,5 +1,6 @@
-//Duplicate Brackets
+
 //https://nados.io/question/duplicate-brackets
+//https://leetcode.com/problems/valid-parentheses/
 class Solution {
     public boolean isValid(String s) {
         
@@ -28,3 +29,43 @@ class Solution {
     
 }
 
+/* //NDOSA
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+     public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner (System.in);
+    String s = scn.nextLine();
+
+      System.out.println(isValid(s));
+    }
+
+   public static boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0 ; i< s.length() ; i++){
+            char ch = s.charAt(i);
+           if((ch >='a'&& ch <='z')|| (ch == ' ')||(ch>='0'&&ch<='9') || (ch=='+')||(ch=='-')||(ch=='/')||(ch=='*')){
+              continue;
+            }
+            else if ( ch == '('||ch=='['||ch=='{'){
+                stack.push(ch);
+            }else {
+                
+                if (stack.isEmpty()) return false ;  
+                char ns = stack.peek();
+                if ( (ch ==')'&&ns=='(')|| (ch ==']' && ns =='[')|| (ch == '}'&&ns=='{')){
+                    stack.pop();
+                }else {
+                    return false ;
+                }
+            }
+                   
+        }
+         return stack.isEmpty();    
+     
+}
+
+} */
