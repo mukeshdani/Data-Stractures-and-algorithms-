c09d9e515ce335830533e5724a4024b9a2ff3eb4
Mukesh Dani <mukeshdani00@gmail.com>
2022-02-12 09:24:05 +0530

    Stack and Queue

---
+++ Contests/FJP-2 Contest(Stack and Queue)/Simple Text Editor.java
@@ -0,0 +1,3 @@
+//simple-text-editor
+//https://www.hackerrank.com/contests/fjp-stack-and-queue/challenges/simple-text-editor/problem
+
---
+++ Contests/FJP-2 Contest(Stack and Queue)/Waiter.java
@@ -0,0 +1,2 @@
+//Waiter
+//https://www.hackerrank.com/contests/fjp-stack-and-queue/challenges/waiter
