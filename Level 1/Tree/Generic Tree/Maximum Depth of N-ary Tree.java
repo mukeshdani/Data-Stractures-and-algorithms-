7ffb5f921a792679ad39870380a6f2fe79371428
Mukesh Dani <mukeshdani00@gmail.com>
2022-02-22 19:56:38 +0530

    tree

---
+++ Level 1/Tree/Generic Tree/Maximum Depth of N-ary Tree.java
@@ -0,0 +1,31 @@
+//Maximum Depth of N-ary Tree
+//https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+      if(root == null ) return 0 ;
+        int height = 0 ;
+        for(Node child : root.children){
+            height = Math.max(height , maxDepth(child));
+        }
+         return 1+height;
+    }
+}
