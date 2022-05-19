a19bc7e112fd708fb0c17ea7b662818d6482ccbc
Mukesh Dani <mukeshdani00@gmail.com>
2022-05-19 22:33:04 +0530

    Tree

---
+++ Level 2/Tree/Binary Tree Inorder Traversal.java
@@ -0,0 +1,52 @@
+//Binary Tree Inorder Traversal
+//https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+//Moris traversal (Inorder)
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/* class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> retVal = new ArrayList<>();
+        
+        TreeNode curr = root;
+        
+        while(curr != null){
+            if(curr.left == null){
+                retVal.add(curr.val);
+                curr = curr.right;
+            }else {
+                TreeNode iop = curr.left;
+                
+                while(iop.right != null && iop.right != curr){
+                    iop = iop.right;
+                }
+                
+                if(iop.right == null){
+                    iop.right = curr;
+                    curr = curr.left;
+                }else {
+                   
+                    iop.right = null;
+                    retVal.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return retVal;
+    }
+} */
