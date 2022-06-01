0067ea151a1f39aed998cd56244d31e0403df4ed
Mukesh Dani <mukeshdani00@gmail.com>
2022-06-02 00:43:01 +0530

    Tree

---
+++ Level 2/Tree/Construct Binary Tree from Preorder and Postorder Traversal.java
@@ -0,0 +1,48 @@
+//Construct Binary Tree from Preorder and Postorder Traversal
+
+//https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i<postorder.length ; i++){
+            map.put(postorder[i],i);
+        }
+        
+        TreeNode root = helper(preorder,postorder , map , 0 , preorder.length-1, 0 , postorder.length -1);
+        return root;
+    }
+    
+   public TreeNode helper(int [] pre , int [] post , HashMap<Integer, Integer> map , int prelo , int prehi , int postlo , int posthi){
+        if(prelo>prehi || postlo > posthi){
+            return null;
+        }
+        
+        TreeNode node = new TreeNode (pre[prelo]);
+        
+        if(prelo + 1 <= prehi){
+            int sidx = map.get(pre[prelo+1]);
+            int lhs = sidx - postlo + 1;
+            
+            node.left = helper(pre , post , map , prelo +1 , prelo + lhs , postlo , sidx);
+            node.right = helper(pre , post , map , prelo + lhs + 1 , prehi , sidx + 1 , posthi -1);
+        }
+        return node;
+    }
+}
