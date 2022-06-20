ab89efbd28be0407dc9c416ed2c502eda758bc5b
Mukesh Dani <mukeshdani00@gmail.com>
2022-06-20 21:15:55 +0530

    Array and String

--- Level 2/Array & String/Chaining Technique/Partition Array into Disjoint Intervals.java
+++ Level 2/Array & String/Chaining Technique/Partition Array into Disjoint Intervals.java
@@ -28,3 +28,26 @@ class Solution {
     }
 }
  */
+
+
+
+ //With out extra space 
+ //o(1) extra space 
+
+ class Solution {
+    public int partitionDisjoint(int[] nums) {
+       int leftmax = nums[0];
+        int greater = nums[0];
+        int ans = 0 ;
+        
+        for(int i = 1; i<nums.length ; i++){
+            if(nums[i] > greater){
+                greater = nums[i];
+            }else if( nums[i] < leftmax){
+                ans = i ;
+                leftmax = greater;
+            }
+        }
+        return ans + 1 ;//length 
+    }
+}
