3591528def629c9c87030f6823a43d178a44324c
Mukesh Dani <mukeshdani00@gmail.com>
2022-08-07 23:35:11 +0530

    Euler Circuit and Path

---
+++ Level 2/Graph/Euler Circuit and Path/(Undirected Graph) Euler circuit and Path.java
@@ -0,0 +1,21 @@
+//Euler circuit and Path
+//https://practice.geeksforgeeks.org/problems/euler-circuit-and-path/1
+
+import java.util.*;
+class Solution
+{
+    public int isEularCircuitExist(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        // Code here
+        int oddCount = 0 ;
+        for(int i = 0 ; i<V ; i++){
+            if(adj.get(i).size() % 2 == 1){
+                oddCount++;
+            }
+        }
+        
+        if(oddCount == 0) return 2; // Circuit Present 
+        if(oddCount == 2) return 1; //Path present but not Circuit
+        return 0; //Not Path Not Circuit Present 
+    }
+}
