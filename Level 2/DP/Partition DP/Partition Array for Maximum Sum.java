2eaeea02a2e0d930eaa49c738c4c285a2efb43c7
Mukesh Dani <mukeshdani00@gmail.com>
2022-10-12 18:17:17 +0530

    MEMO

---
+++ Level 2/DP/Partition DP/Partition Array for Maximum Sum.java
@@ -0,0 +1,28 @@
+//Partition Array for Maximum Sum
+//https://leetcode.com/problems/partition-array-for-maximum-sum/
+
+//Memoization 
+class Solution {
+    public int F(int idx , int n , int [] arr , int k , int [] dp){
+        if(idx == n)return 0;
+        if(dp[idx]!=-1)return dp[idx];
+        int maxAns = Integer.MIN_VALUE;
+        int len = 0 ;
+        int maxi = Integer.MIN_VALUE;
+        
+        for(int i = idx ; i < Math.min(n , idx + k) ; i++){
+            len++;
+            maxi = Math.max(maxi , arr[i]);
+            int sum = len * maxi + F(i+1 , n , arr , k , dp);
+            maxAns = Math.max(maxAns , sum);
+        }
+        return dp[idx] = maxAns;
+    }
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int [] dp = new int [arr.length];
+        for(int i = 0 ; i< dp.length ; i++){
+            dp[i] = -1;
+        }
+        return F(0 , arr.length , arr , k , dp);
+    }
+}
