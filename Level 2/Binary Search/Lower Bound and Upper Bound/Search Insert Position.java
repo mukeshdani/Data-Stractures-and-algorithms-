899bc52ae20a791fd91794ffa22f9def6fb0948b
Mukesh Dani <mukeshdani00@gmail.com>
2022-11-18 08:49:33 +0530

    Added Lower Bound and upper bound

---
+++ Level 2/Binary Search/Lower Bound and Upper Bound/Search Insert Position.java
@@ -0,0 +1,60 @@
+//Search Insert Position
+// Lower Bound: Search Insert Position: Leetcode 35
+// https://leetcode.com/problems/search-insert-position/description/
+
+// Time = O(log2 N), Space = O(1)
+
+class Solution {
+    public static int lowerBound(int[] nums, int target) {
+        // Same as First Occurence
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] >= target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+
+    public static int upperBound(int[] nums, int target) {
+        // Same as Ceil Value
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+}
+
+
+
+
+//LC - Solution 
+class Solution2{
+    public int searchInsert(int[] nums, int target) {
+        int left = 0 , right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else {
+                right = mid - 1 ;
+            }
+        }
+        return left;
+    }
+}
