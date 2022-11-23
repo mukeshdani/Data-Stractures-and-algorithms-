b7e0a759ea6cfc12f6e6db739d0db21b1bf66f59
Mukesh Dani <mukeshdani00@gmail.com>
2022-11-23 10:49:40 +0530

    StepArraySearch

---
+++ Level 2/Binary Search/StepArraySearch.java
@@ -0,0 +1,32 @@
+//StepArraySearch
+// https://practice.geeksforgeeks.org/problems/searching-in-an-array-where-adjacent-differ-by-at-most-k0456/1
+
+
+// Time = O(N) Linear Search Worst Case, O(N / K) Avg Case
+
+// Using Jump Search Algorithm
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int search (int arr[], int n, int target, int jump) {
+        //Complete the function
+        int idx = 0;
+        
+        while(idx<n){
+            
+            if(arr[idx] == target)return idx;
+            
+            int requiredJump = Math.abs(arr[idx]  - target);
+            int minJump  = requiredJump /  jump;//floor
+            if(requiredJump % jump !=0)minJump++;//ceil
+            
+            idx = idx + minJump;
+        }
+          return -1;
+    }
+  
+    
+}
