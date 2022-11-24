a69b9379d82ed5d33f5bf47247e46ff70788b6d5
Mukesh Dani <mukeshdani00@gmail.com>
2022-11-24 10:13:19 +0530

    Binary Search Completed

--- Level 2/Binary Search/Binaery Search On Answer/MissingelementofAP.java
+++ Level 2/Binary Search/Binaery Search On Answer/MissingelementofAP.java
@@ -5,35 +5,35 @@
 class Solution {
     public static int findMissing(int[] arr, int n) {
         // Common Difference
-  int d = (arr[n - 1] - arr[0]) / n;
-  int low = 0;
-  int high = n - 1;
+      int d = (arr[n - 1] - arr[0]) / n;
+      int low = 0;
+      int high = n - 1;
 
-  while (low <= high) {
-    // Find the middle index
-    int mid = low + (high - low) / 2;
+      while (low <= high) {
+        // Find the middle index
+        int mid = low + (high - low) / 2;
 
-    // if mid and the immediate next element to mid are not in AP, then missing
-    // element is arr[mid] + d.
-    if (arr[mid + 1] - arr[mid] != d)
-      return arr[mid] + d;
+        // if mid and the immediate next element to mid are not in AP, then missing
+        // element is arr[mid] + d.
+        if (arr[mid + 1] - arr[mid] != d)
+          return arr[mid] + d;
 
-    // if mid and the immediate previous element to mid are not in AP, then missing
-    // element is arr[mid-1] + d.
-    if (mid > 0 && arr[mid] - arr[mid - 1] != d)
-      return arr[mid - 1] + d;
+        // if mid and the immediate previous element to mid are not in AP, then missing
+        // element is arr[mid-1] + d.
+        if (mid > 0 && arr[mid] - arr[mid - 1] != d)
+          return arr[mid - 1] + d;
 
-    // if elements of the left half are in AP, then missing element is in right
-    // half.
-    if (arr[mid] == arr[0] + mid * d)
-      low = mid + 1;
+        // if elements of the left half are in AP, then missing element is in right
+        // half.
+        if (arr[mid] == arr[0] + mid * d)
+          low = mid + 1;
 
-    // else missing element is in the left half.
-    else
-      high = mid - 1;
-  }
-return -1;
-}
+        // else missing element is in the left half.
+        else
+          high = mid - 1;
+      }
+    return -1;
+    }
 }
 
 
