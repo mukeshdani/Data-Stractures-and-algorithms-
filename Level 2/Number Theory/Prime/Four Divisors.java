cf24db53e3f9f8417a993eaa740aa4eb3cb80816
Mukesh Dani <mukeshdani00@gmail.com>
2022-11-27 11:31:09 +0530

    Four Divisors

---
+++ Level 2/Number Theory/Prime/Four Divisors.java
@@ -0,0 +1,28 @@
+//Four Divisors
+///https://leetcode.com/problems/four-divisors/
+
+class Solution {
+    public int getFactor(int n){
+        int count = 0 , sum =0 ;
+        for(int i = 1 ; i*i<=n; i++){
+            if(n%i == 0){
+                count++;
+                sum += i;
+                if(i != n/i){
+                    sum+=n/i;
+                    count++;
+                    
+                }
+            }
+        }
+        if(count == 4)return sum;
+        return 0;
+    }
+    public int sumFourDivisors(int[] nums) {
+        int sum =0 ;
+        for(int num : nums){
+              sum+=getFactor(num);
+        }
+        return sum;
+    }
+}
