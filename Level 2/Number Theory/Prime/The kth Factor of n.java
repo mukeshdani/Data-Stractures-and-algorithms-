ffeb3e3cc7e8f23dd91fd2a32cbce04fa755b81f
Mukesh Dani <mukeshdani00@gmail.com>
2022-11-27 11:31:17 +0530

    Four Divisors

---
+++ Level 2/Number Theory/Prime/The kth Factor of n.java
@@ -0,0 +1,29 @@
+//The kth Factor of n
+//https://leetcode.com/problems/the-kth-factor-of-n/
+//
+
+
+import java.util.*;
+class Solution {
+    public int kthFactor(int n, int k) {
+        List<Integer> res = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        
+        for(int factor = 1 ; factor * factor <= n ; factor ++){
+            if(n % factor == 0){
+                res.add(factor);
+                if(factor != n / factor)right.add(n/factor);
+                    
+            }
+        }
+        
+        Collections.reverse(right);
+        res.addAll(right);
+        
+        if(k > res.size()){
+            return -1;
+        }
+        return res.get(k-1);
+    }
+}
+
