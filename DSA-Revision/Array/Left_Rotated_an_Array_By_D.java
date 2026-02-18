5563bcfefe2b70a92add1cb6a9bbc6f9b356f21f
mukeshdani00 <mukeshdani00@gmail.com>
2026-02-18 21:39:21 +0530

    Optimal Approach

--- DSA-Revision/Array/Left_Rotated_an_Array_By_D.java
+++ DSA-Revision/Array/Left_Rotated_an_Array_By_D.java
@@ -16,14 +16,36 @@ public class Left_Rotated_an_Array_By_D {
             d--;
         }
     }
-    public static void optimalApproach(int arr[], int d){
+    public static void optimalApproach(int arr[], int d) {
+        int n = arr.length;
+        int temp[] = new int[d];
 
+        // Step 1: Copy first d elements into temp
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+
+        int first = 0;
+        int second = d;
+
+        // Step 2: Shift elements left by swapping
+        while (second < n) {
+            arr[first] = arr[second];
+            first++;
+            second++;
+        }
+
+        // Step 3: Put temp[] elements at the end
+        for (int i = 0; i < d; i++) {
+            arr[first + i] = temp[i];
+        }
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
         int d = 10;
         d=d%arr.length;
-        bruitForce(arr, d);
+        //bruitForce(arr, d);
+        optimalApproach(arr, d);
         for(int i = 0 ; i<arr.length; i++){
             System.out.print(arr[i] + " ");
         }
